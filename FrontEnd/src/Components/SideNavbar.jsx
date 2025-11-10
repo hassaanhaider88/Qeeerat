f593deb327aa4f66827a2aa669de065432968b22
hassaanhaider88 <hassaanhaider088@gmail.com>
2025-11-10 21:12:12 +0500

    add sign-up and login UI

--- FrontEnd/src/Components/SideNavbar.jsx
+++ FrontEnd/src/Components/SideNavbar.jsx
@@ -1,68 +1,74 @@
-import { BiDotsVertical } from 'react-icons/bi'
-import { FaRegUser } from 'react-icons/fa'
-import { GoHome } from 'react-icons/go'
-import { IoMdAdd } from 'react-icons/io'
-import { IoNotificationsOutline, IoSettings } from 'react-icons/io5'
-import { MdOutlineExplore, MdOutlineWorkspacePremium } from 'react-icons/md'
-import { Link } from 'react-router-dom'
+import { FaRegUser } from "react-icons/fa";
+import { GoHome } from "react-icons/go";
+import { IoMdAdd } from "react-icons/io";
+import { IoNotificationsOutline, IoSettings } from "react-icons/io5";
+import { MdOutlineExplore, MdOutlineWorkspacePremium } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const SideNavbar = () => {
+ 
   const SidebarOptions = [
     {
-      Name : "Home",
-      Icon : <GoHome />,
-      redirectTo : "/"
+      Name: "Home",
+      Icon: <GoHome />,
+      redirectTo: "/",
     },
     {
-      Name : "Explore",
-      Icon : <MdOutlineExplore />,
-      redirectTo : "/explore"
+      Name: "Explore",
+      Icon: <MdOutlineExplore />,
+      redirectTo: "/explore",
     },
     {
-      Name : "Notification",
-      Icon : <IoNotificationsOutline />,
-      redirectTo : "/notification"
+      Name: "Notification",
+      Icon: <IoNotificationsOutline />,
+      redirectTo: "/notification",
     },
     {
-    Name : "Add New",
-              Icon : <IoMdAdd />,
-              redirectTo : "/add-new"
-            },
-    {
-Name : "Premium",
-Icon : <MdOutlineWorkspacePremium />,
-redirectTo : "/premium"
-
+      Name: "Add New",
+      Icon: <IoMdAdd />,
+      redirectTo: "/add-new",
     },
     {
-      Name : "Profile",
-      Icon : <FaRegUser />,
-      redirectTo : "/profile"
-    }
-  ]
+      Name: "Premium",
+      Icon: <MdOutlineWorkspacePremium />,
+      redirectTo: "/premium",
+    },
+    {
+      Name: "Profile",
+      Icon: <FaRegUser />,
+      redirectTo: "/profile",
+    },
+  ];
   return (
-    <div className='lg:w-[25%] sticky border-r-2 border-gray-500 overflow-hidden -top-1 sm:flex flex-col justify-between h-screen md:w-[32%] text-nowrap text-white py-3 px-5 hidden  w-0 sm:w-[150px] bg-black'>
-       <div>
+    <div className="lg:w-[25%] sticky border-r-2 border-gray-500 overflow-hidden -top-1 sm:flex flex-col justify-between h-screen md:w-[32%] text-nowrap text-white py-3 px-5 hidden  w-0 sm:w-[150px] bg-black">
+      <div>
         <div className="LogoGoesHere flex justify-center items-center md:justify-start w-full">
-        <img className='h-30 ' src="./QeeeratLogoWithBG.png" alt="" />
-       </div>
-      <div className='w-full flex gap-2 py-3 flex-col'>
-     {
-      SidebarOptions.map((option,idx)=>{
-   return <Link key={idx} to={option.redirectTo}  className="flex text-2xl w-full gap-4 hover:bg-[#da55e69f] duration-300 transition-all cursor-pointer py-3 px-2 justify-center md:justify-start items-center rounded-4xl">
-      <span title={option.Name}>{option.Icon}</span>
-      <h1 className='hidden md:block'>{option.Name}</h1>
-     </Link>
-      })
-     }
+          <img className="h-30 " src="./QeeeratLogoWithBG.png" alt="" />
+        </div>
+        <div className="w-full flex gap-2 py-3 flex-col">
+          {SidebarOptions.map((option, idx) => {
+            return (
+              <Link
+                key={idx}
+                to={option.redirectTo}
+                className="flex text-2xl w-full gap-4 hover:bg-[#da55e69f] duration-300 transition-all cursor-pointer py-3 px-2 justify-center md:justify-start items-center rounded-4xl"
+              >
+                <span title={option.Name}>{option.Icon}</span>
+                <h1 className="hidden md:block">{option.Name}</h1>
+              </Link>
+            );
+          })}
+        </div>
       </div>
-       </div>
-       {/* show only when user is logged in  */}
-       <Link to={'/settings'} className='flex mb-2 cursor-pointer flex-col md:flex-row gap-3 w-full justify-start items-center'>
-        <IoSettings size={'25'}/>
-       </Link>
+      {/* show only when user is logged in  */}
+      <Link
+        to={"/settings"}
+        className="flex mb-2 cursor-pointer flex-col md:flex-row gap-3 w-full justify-start items-center"
+      >
+        <IoSettings size={"25"} />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default SideNavbar
+export default SideNavbar;
--- FrontEnd/src/Pages/Home.jsx
+++ FrontEnd/src/Pages/Home.jsx
@@ -1,52 +1,65 @@
-import React, { useEffect } from 'react'
-import SideNavbar from '../Components/SideNavbar'
-import HomeFeed from './HomeFeed'
-import { Route, Routes } from 'react-router-dom'
-import Explore from './Explore'
-import Notification from './Notification'
-import Profile from './Profile'
-import MobileBar from '../Components/MobileBar'
-import Premium from './Premium'
-import Login from './Login'
-import SignUp from './SignUp'
-import Settings from './Settings'
+import React, { useEffect, useState } from "react";
+import SideNavbar from "../Components/SideNavbar";
+import HomeFeed from "./HomeFeed";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Explore from "./Explore";
+import Notification from "./Notification";
+import Profile from "./Profile";
+import MobileBar from "../Components/MobileBar";
+import Premium from "./Premium";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import Settings from "./Settings";
 import useUserData from "../store/useUserData";
-import AddNewPost from './AddNewPost'
-import EditProfile from './EditProfile'
-import ChangePass from './ChangePass'
-import ChangeEmail from './ChangeEmail'
+import AddNewPost from "./AddNewPost";
+import EditProfile from "./EditProfile";
+import ChangePass from "./ChangePass";
+import ChangeEmail from "./ChangeEmail";
 
 const HomeWithLogin = () => {
-const {userData,setUserData} = useUserData();
-useEffect(()=>{
-  var  StoreUser = JSON.parse(localStorage.getItem("QeeeratUserData"))
-  console.log(StoreUser)
-  
-  
-},[])
+  const { userData, setUserData } = useUserData();
+  const [IsShowSideBar, setIsShowSideBar] = useState(true);
+  const Location = useLocation();
+  console.log(Location.pathname);
+  useEffect(() => {
+    if (Location.pathname == "/sign-up" || Location.pathname == "/login") {
+      setIsShowSideBar(false);
+    } else {
+      setIsShowSideBar(true);
+    }
+  }, [Location.pathname]);
+  useEffect(() => {
+    var StoreUser = JSON.parse(localStorage.getItem("QeeeratUserData"));
+    console.log(StoreUser);
+  }, []);
 
   return (
-    <div className='w-full min-h-screen flex'>
-     <SideNavbar/>
-     <MobileBar/>
-     <Routes>
-      <Route path='/' element={<HomeFeed/>} />
-      <Route path='/explore' element={<Explore/>}/>
-      <Route path='/notification' element={<Notification/>}/>
-      <Route path='/profile' element={<Profile/>}/>
-      <Route path='/edit-profile' element={<EditProfile/>}/>
-      <Route path='/add-new' element={<AddNewPost/>}/>
-      <Route path='/premium' element={<Premium/>}/>
-      <Route path='/settings' element={<Settings/>} />
-      <Route path='/change-pass' element={<ChangePass/>} />
-      <Route path='change-email' element={<ChangeEmail/>} />
-      <Route path='/login' element={<Login/>} />
-      <Route path='/sign-up' element={<SignUp/>} />
+    <div className="w-full min-h-screen flex">
+      {IsShowSideBar && (
+        <>
+          <MobileBar />
+          <SideNavbar />
+        </>
+      )}
 
-      <Route/>
-     </Routes>
+      <Routes>
+        <Route path="/" element={<HomeFeed />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/notification" element={<Notification />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+        <Route path="/add-new" element={<AddNewPost />} />
+        <Route path="/premium" element={<Premium />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/change-pass" element={<ChangePass />} />
+        <Route path="change-email" element={<ChangeEmail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+
+        <Route />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default HomeWithLogin
+export default HomeWithLogin;
